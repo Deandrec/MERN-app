0c8352255746cfb4ace3c0f6e8dd3149e0f00433
Deandre Clark <Deandreclark123@@gmail.com>
2022-12-22 10:18:48 -0500

    redux config

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,2 +1,55 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initalState = {
+    mode: "light",
+    user: null,
+    token: null,
+    posts: [],
+}
+
+export const authSlice = createSlice({
+    name:"auth",
+    initalState,
+
+    // functions that change the global state
+    reducers : {
+        setMode: (state) =>{
+            state.mode = state.mode === "light" ? "dark": "light";
+        },
+
+        setLogin: (state, action) =>{
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+
+        setLogout: (state) =>{
+            state.user = null;
+            state.token = null;
+        },
+
+        setFriends: (state, action) =>{
+            if(state.user){
+                state.user.friends = action.payload.friends
+            }else{
+                console.error("User Friends Non-existent")
+            }
+        },
+
+        setPosts: (state, action) =>{
+            state.posts = action.payload.posts
+        },
+        setPost: (state, action) =>{
+            const updatedPosts = state.posts.map((post)=>{
+                if(post._id === action.payload._id) return action.payload.post;
+                return post;
+            })
+
+            state.post = updatedPosts;
+        }
+
+    }
+});
+
+export const {setMode, setLogin, setLogout, setFriends, setPosts, setPost} = authSlice.actions;
+
+export default authSlice.reducer; 
